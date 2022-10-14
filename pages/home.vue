ee92d7e82d6e874fd4eab38c03e8e868e9e9c3e7
pea-rl <irishsumicad84@gmail.com>
2022-10-14 11:23:06 +0800

    today's activity

--- pages/home.vue
+++ pages/home.vue
@@ -2,12 +2,14 @@
   <div>
     <h2>Home Page</h2>
 
-    <div class="card" style="width: 18rem;">
-      <img src="~/static/images/CTUlogo.png" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <NuxtLink to="/dashboard" class="btn btn-primary">Dashboard</NuxtLink>
+    <div class="center">
+      <div class="card" style="width: 18rem;">
+        <img src="~/static/images/CTUlogo.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <NuxtLink to="/dashboard" class="btn btn-primary">Dashboard</NuxtLink>
+        </div>
       </div>
     </div>
 
@@ -21,5 +23,11 @@ export default {
 </script>
 
 <style>
-
+.center {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    padding: 2%;
+    position: relative;
+}
 </style>
