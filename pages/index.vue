37f4dee23ace5f93556ea8a128f0f2e6653675c8
pea-rl <irishsumicad84@gmail.com>
2022-10-14 08:06:18 +0800

    add

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fluid">
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Login from "./login.vue";
+export default {
+    name: "IndexPage",
+    components: { Login }
+}
+</script>
