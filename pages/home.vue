34d2ead600259c1c7de62181a0c8a3ed57a4c6f9
pea-rl <irishsumicad84@gmail.com>
2022-10-14 10:48:39 +0800

    sampleContent

--- pages/home.vue
+++ pages/home.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h2>Home Page</h2>
+
+    <div class="card" style="width: 18rem;">
+      <img src="~/static/images/CTUlogo.png" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <NuxtLink to="/dashboard" class="btn btn-primary">Dashboard</NuxtLink>
+      </div>
+    </div>
+
   </div>
 </template>
 
