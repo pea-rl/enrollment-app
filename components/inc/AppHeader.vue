8197f158cbfbe9975f1a48c6768aa43651c6a820
pea-rl <irishsumicad84@gmail.com>
2022-10-14 08:02:33 +0800

    "commt"

---
+++ components/inc/AppHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+  <nav class="nav navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbrand" href="#">CTU Enrollment</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!--
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <NuxtLink to="/login" class="nav-link">Login</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    -->
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style>
+.nav {
+  background-color: #B22727;
+  height: 60px;
+  align-items: center;
+  width: 100%;
+}
+.navbrand {
+  font-weight: 700;
+  align-items: flex-start;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: large;
+  color: #F7F5F2;
+  
+}
+
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container-fluid">
+    <form class="center">
+      <div class="card cardbg">
+        
+        <div class="curved-background">
+          <div class="curved-background__curved">
+            <div class="center">
+              <img src="~/static/images/CTUlogo.png" class="logo" />
+            </div>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <h4 class="center">Login</h4>
+
+          <div class="mb-3 form-check">
+            <div class="floating-label">
+              <input type="email" class="floating-label__input" id="floatingInput" placeholder="Email">
+              <label for="floatingInput" class="floating-label__label">Email</label>
+            </div>
+          </div>
+
+          <div class="mb-3 form-check">
+            <div class="floating-label">
+              <input type="password" class="floating-label__input" id="floatingPassword" placeholder="Password">
+              <label for="floatingInput" class="floating-label__label">Password</label>
+            </div>
+          </div>
+
+          <div class="center">
+            <button type="submit" class="btnLogin">LOGIN</button>
+          </div>
+        </div>
+      </div>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+.curved-background__curved {
+    /* Background color */
+    background-color: #000000;
+
+    /* You can use gradient background color such as */
+    /* background: linear-gradient(rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.1) 100%); */
+
+    /* Curved corners */
+    border-bottom-left-radius: 50% 40%;
+    border-bottom-right-radius: 50% 40%;
+
+    /* Size */
+    height: 100%;
+    width: 100%;
+}
+.center {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    padding: 5%;
+    position: relative;
+}
+.logo {
+    /* Size */
+    height: 30%;
+    width: 30%;
+}
+.btnLogin {
+  font-weight: 600;
+  border-radius: 8px;
+  padding: 2%;
+  width: 150px;
+  height: 40px;
+  border: 1px solid #d1d5db;
+}
+.floating-label {
+  border: 1px solid #d1d5db;
+  border-radius: 0.25rem;
+  position: relative;   
+}
+.floating-label__input {
+  border: none;
+  padding: 0.5rem;
+  height: 100%;
+  width: 100%;
+}
+.floating-label__label {
+  /* Position the label */
+  left: 1rem;
+  position: absolute;
+  top: 100%;
+  /* Hide it by default */
+  opacity: 0;
+  transition: all 200ms;
+  padding: 0.5rem;
+}
+
+.cardbg {
+  background-color: #FF9F29;
+  position: relative;
+  width: 400px;
+  box-shadow: 10px 11px 10px 2px #D8D9CF;
+}
+
+</style>
